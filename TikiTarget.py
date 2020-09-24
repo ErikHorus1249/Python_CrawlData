b0cf1760d65317731abe121dbfc3b91d58c134e9
ErikHorus <tuananh1421999@gmail.com>
2020-09-24 17:57:57 +0700

    up

--- TikiTarget.py
+++ TikiTarget.py
@@ -3,8 +3,16 @@
 class TikiTarget:
     def __init__(self, patternsStr="", categoryStr=""):
         self.patternsString = patternsStr 
-        self.patterns = []
+        self.patterns = self.__SplitPattern()
         self.categoryUrl = categoryStr
 
     def info(self):
-        return "patterns : " + self.patternsString + "categoriesUrl : "+self.categoryUrl
+        return "patterns : " + str(self.patterns[1]) + "categoriesUrl : "+self.categoryUrl
+    
+    def __SplitPattern(self):
+        newList = self.patternsString.split(",")
+        i = 0
+        while i < len(newList):
+            newList[i] = newList[i].strip()
+            i += 1
+        return newList
